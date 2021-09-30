79c5cf1cd6f2332604588a118f24ef3c504aa948
shereenashajahan01 <80378536+shereenashajahan01@users.noreply.github.com>
2021-10-01 02:05:30 +0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 import { LoggingInterceptor } from './logger.inteceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{ cors: true });
+  const app = await NestFactory.create(AppModule,{cors: {
+    origin: true,
+    preflightContinue: false,
+  }});
   
   await app.listen(process.env.PORT || 3000);
 }
